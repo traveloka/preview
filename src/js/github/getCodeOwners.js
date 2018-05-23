3dc8a885a3e34f438434a148b05827bf0197279c
Fatih Kalifa <fatihkalifa@gmail.com>
2018-05-23 20:29:33 +0700

    use default branch
    fix #3

--- src/js/github/getCodeOwners.js
+++ src/js/github/getCodeOwners.js
@@ -1,10 +1,12 @@
 import domFind from "../content/utils/domFind";
 import fetchHtml from "./fetchHtml";
 import mapCodeOwners from "./mapCodeOwners";
+import getDefaultBranch from "./getDefaultBranch";
 
 export default async function getCodeOwners(repo) {
   try {
-    const html = await fetchHtml("/blob/master/.github/CODEOWNERS", repo);
+    const branch = await getDefaultBranch(repo);
+    const html = await fetchHtml(`/blob/${branch}/.github/CODEOWNERS`, repo);
     const codeowners = domFind(
       html,
       root => root.querySelector(".blob-wrapper table").innerText
