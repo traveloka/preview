55363758e7fb4d7696b76cf1e04ec0dede37b4ff
Fatih Kalifa <fatihkalifa@gmail.com>
2018-05-24 15:17:13 +0700

    fix noop on history state update to /files

--- src/js/background.js
+++ src/js/background.js
@@ -1,21 +1,14 @@
 import "../img/icon-128.png";
+import isFilesSection from "./utils/isFilesSection";
+import { HISTORY_STATE_UPDATE } from "./utils/events";
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
   if (isFilesSection(details.url)) {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      notifyContentScript(tabs, details.url);
+      chrome.tabs.sendMessage(tabs[0].id, {
+        event: HISTORY_STATE_UPDATE,
+        url: details.url
+      });
     });
   }
 });
-
-const notifyContentScript = (tabs, triggerUrl) =>
-  tabs[0] &&
-  chrome.tabs.sendMessage(tabs[0].id, {
-    preview: "background",
-    location: triggerUrl
-  });
-
-const isFilesSection = url =>
-  url &&
-  url.indexOf("github.com") > 0 &&
-  url.replace(/\?.*/i, "").endsWith("/files");
