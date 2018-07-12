02f4877bd720c2c6e0e1bb3f152299fd77b83580
fchristysen <fchristysen@gmail.com>
2018-07-12 23:08:46 +0700

    Add Feature : Show updated "Change Requested PR" (#8)
    Add feature to show notification indicator(like unread PR which is blue but orange). The extension now active on Pull Request page (e.g. : https://github.com/traveloka/android-v3/pulls), then it will check each changed requested PR. If there is new commit (from the author of PR) after the latest change request, the PR item will be flagged with vertical orange bar. 
    Test Plan : https://github.com/traveloka/android-playground/pulls. Should contain 1 PR that flagged orange

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,10 @@
 import "../img/icon-128.png";
 import isFilesSection from "./utils/isFilesSection";
+import isPullsSection from "./utils/isPullsSection";
 import { HISTORY_STATE_UPDATE } from "./utils/events";
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  if (isFilesSection(details.url)) {
+  if (isFilesSection(details.url) || isPullsSection(details.url)) {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
         event: HISTORY_STATE_UPDATE,
