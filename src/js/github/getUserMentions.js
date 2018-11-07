63583f80811c9e23cc4370ab518319effba2ef18
Fatih Kalifa <fatihkalifa@gmail.com>
2018-11-07 14:06:39 +0700

    fix user team detection for the one using old gh dashboard

--- src/js/github/getUserMentions.js
+++ src/js/github/getUserMentions.js
@@ -3,12 +3,17 @@ import domFind from "../content/utils/domFind";
 
 export default async function getUserMentions(repo) {
   const html = await fetchHtml("/");
-  const teams = domFind(
-    html,
-    root =>
-      root.querySelector("#your-teams-filter").parentNode.parentNode.children[2]
-        .innerText
-  )
+  const teams = domFind(html, root => {
+    const betaDashboardTeamsNode = root.querySelector("#your-teams-filter")
+      .parentNode.parentNode.children[2];
+    if (betaDashboardTeamsNode) {
+      return betaDashboardTeamsNode.innerText;
+    }
+
+    // user maybe still using old dashboard
+    return root.querySelector("[data-filterable-for='your-teams-filter']")
+      .innerText;
+  })
     .split("\n")
     .filter(Boolean)
     .map(t => t.trim())
@@ -17,10 +22,17 @@ export default async function getUserMentions(repo) {
 
   const user =
     "@" +
-    domFind(
-      html,
-      root => root.querySelector(".account-switcher span").innerText
-    ).trim();
+    domFind(html, root => {
+      const betaDashboardAccountSwitcher = root.querySelector(
+        ".account-switcher span"
+      );
+      if (betaDashboardAccountSwitcher) {
+        return betaDashboardAccountSwitcher.innerText;
+      }
+
+      return root.querySelector(".account-switcher-truncate-override")
+        .innerText;
+    }).trim();
 
   return teams.concat([user]);
 }
