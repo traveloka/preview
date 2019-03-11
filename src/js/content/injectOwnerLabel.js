0b038669baccf7301e67481b1e2d1dabef613ea0
Fatih Kalifa <fatihkalifa@gmail.com>
2019-03-11 17:35:18 +0700

    better reviewer label

--- src/js/content/injectOwnerLabel.js
+++ src/js/content/injectOwnerLabel.js
@@ -17,10 +17,22 @@ export default function injectOwnerLabel(header, owners) {
     title.textContent = owners.length === 1 ? "Owner: " : "Owners: ";
     fragment.appendChild(title);
 
-    owners.forEach(owner => {
-      const span = document.createElement("span");
-      span.textContent = owner;
-      fragment.appendChild(span);
+    owners.forEach((owner, index) => {
+      const reviewer = document.createElement("a");
+      const separator = document.createTextNode(", ");
+      const reviewerLink = owner.includes("/")
+        ? "orgs/" +
+          owner
+            .replace("@", "")
+            .split("/")
+            .join("/teams/")
+        : owner.replace("@", "");
+      reviewer.textContent = owner;
+      reviewer.href = "https://github.com/" + reviewerLink;
+      fragment.appendChild(reviewer);
+      if (index < owners.length - 1) {
+        fragment.appendChild(separator);
+      }
     });
 
     labelContainer.appendChild(fragment);
